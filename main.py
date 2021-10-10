0a2c5bb4c74948bb9c4e9b7b29e6b20e153fcb81
KamranKaza <kamrankaza@gmail.com>
2021-10-10 16:03:51 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,21 @@
+import discord
+from discord.ext import commands
+intents = discord.Intents.default()
+intents.messages = True
+
+bot = commands.Bot(command_prefix='-', intents=intents)
+
+@bot.event
+async def on_message_delete(message):
+    with open('snipe.txt', "w") as myfile:
+        myfile.write(message.content)
+    with open('snipe2.txt', "w") as myfile:
+        myfile.write(str(message.author.id))
+
+@bot.command()
+async def snipe(ctx):
+    my_file = open("snipe.txt", "r")
+    my_file2 = open("snipe2.txt", "r")
+    await ctx.reply('<@' + my_file2.read() + '>: ' + my_file.read())
+
+bot.run('ODk2NTc4NjIxMTEwNjE2MDY1.YWJJ6A.mPog9L3Nzpadd-LVZBXiFX2BfNs')
